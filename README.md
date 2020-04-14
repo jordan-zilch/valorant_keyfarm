181ca8eddccf490eaf6bba38490e42e5ec67dd0a
lozinski <lozinski@umich.edu>
2020-04-14 03:45:49 -0400

    stuff

--- README.md
+++ README.md
@@ -4,9 +4,10 @@
 
 * Python 3.7 or higher
 * twitch-python (`pip install --user twitch-python`)
+* Google Chrome
 
 ## Usage
 
 Clone or download this repo and `python3 valorant.py {CLIENT_ID}`. `{CLIENT_ID}` is your client id from the Twitch API - if you don't have one already you can get one [here](https://dev.twitch.tv/docs/v5#getting-a-client-id).
 
-
+If you want to use a different browser than Chrome, you can provide a second argument by providing one of the typenames from [this table](https://docs.python.org/3/library/webbrowser.html#webbrowser.register).
--- valorant.py
+++ valorant.py
@@ -5,6 +5,14 @@ import time
 import os
 import sys
 
+def usage():
+    print("Either:\nvalorant.py {CLIENT_ID}\nvalorant.py {CLIENT_ID} {WEB_BROWSER}")
+    print("See README.md")
+
+if len(sys.argv) < 2 or len(sys.argv) > 3:
+    usage()
+    sys.exit()
+
 client_id=sys.argv[1]
 DROPSENABLED_TAGID = "c2542d6d-cd10-4532-919b-3d19f30a768b"
 
@@ -13,6 +21,7 @@ def main():
         stream_to_watch = ""
         stream_to_watch_id = -1
 
+        # Find a drops-enabled Valorant stream
         helix = twitch.Helix(client_id=client_id)
         for stream in helix.streams(game_id=516575):
             if DROPSENABLED_TAGID in stream.data['tag_ids']:
@@ -27,15 +36,15 @@ def main():
                 break
         print("watching " + stream_to_watch)
 
-
+        # Watch the stream until it isnt live
         if stream_to_watch != "":
             live = True
             while live:
                 print(stream_to_watch + " still live")
-                # Heartbeat every five minutes to check that its live
+                # Heartbeat every 20 seconds to check that its live
                 b = helix.streams(user_id=stream_to_watch_id)
                 live = b._data[0].type == "live"
-                time.sleep(10)
+                time.sleep(20)
             os.system("pkill chrome")
 
 
